60f10c3582added9980d4f84415faae8ab8b533c
Qneil Arrieta <arrietaqneil999@gmail.com>
2022-04-21 12:28:09 +0800

    Add color selector to ethc-a-sketch

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 function clearGrid(){
-    let cells = document.querySelectorAll(".colored");
+    let cells = document.querySelectorAll(".cells");
     cells.forEach(cell  => {
-        cell.classList.remove("colored");
+        cell.style.backgroundColor = `rgb(255,255,255)`;
     });
 }
 let slider = document.getElementById("myRange");
 let output = document.getElementById("value");
+let rSlider = document.getElementById("RRange");
+let rOutput = document.getElementById("Rvalue");
+rOutput.innerHTML = rSlider.value;
+rSlider.oninput = function(){ rOutput.innerHTML = this.value};
+rSlider.addEventListener("change", function(){
+    let r = rSlider.value;
+    let rColor = `rgb(${r}, ${gSlider.value}, ${bSlider.value})`;
+    rSlider.style.background = rColor;
+    gSlider.style.background = rColor;
+    bSlider.style.background = rColor;
+});
+let gSlider = document.getElementById("GRange");
+let gOutput = document.getElementById("Gvalue");
+gOutput.innerHTML = gSlider.value;
+gSlider.oninput = function(){ gOutput.innerHTML = this.value};
+gSlider.addEventListener("change", function(){
+    let g = gSlider.value;
+    let gColor = `rgb(${rSlider.value}, ${g}, ${bSlider.value})`;
+    gSlider.style.background = gColor;
+    rSlider.style.background = gColor;
+    bSlider.style.background = gColor;
+});
+let bSlider = document.getElementById("BRange");
+let bOutput = document.getElementById("Bvalue");
+bOutput.innerHTML = bSlider.value;
+bSlider.oninput = function(){ bOutput.innerHTML = this.value};
+bSlider.addEventListener("change", function(){
+    let b = bSlider.value;
+    let bColor = `rgb(${rSlider.value}, ${gSlider.value}, ${b})`;
+    bSlider.style.background = bColor;
+    rSlider.style.background = bColor;
+    gSlider.style.background = bColor;
+});
 output.innerHTML = `${slider.value}`+`x`+`${slider.value}`;
-
 slider.oninput = function(){ output.innerHTML = `${this.value}`+ `x`+ `${this.value}`};
 let x = slider.value;
 let grid = `repeat(` + x + `, 1fr)`;
@@ -18,20 +50,16 @@ let toAdd = document.createDocumentFragment();
 for(let i=0; i < (x*x); i++)
 {
 let newDiv = document.createElement('div');
-newDiv. addEventListener("mouseover", () => newDiv.classList.add("colored"));
-newDiv.className = 'cells';
-toAdd.appendChild(newDiv);
+newDiv. addEventListener("mouseover", () =>  newDiv.style.backgroundColor = `rgb(${rSlider.value}, ${gSlider.value}, ${bSlider.value})`);
+    newDiv.className = 'cells';
+    toAdd.appendChild(newDiv);
 }
 container.appendChild(toAdd);
-
 slider.addEventListener("change", function(){
     let x = slider.value;
     let color = `linear-gradient(90deg, rgb(166, 75, 42)` + x + `%, rgb(214, 214, 214)` + x + `%)`;
     slider.style.background = color;
-    const cells = document.querySelectorAll(".colored");
-    cells.forEach(cell  => {
-        cell.classList.remove("colored");
-    });
+    clearGrid();
     let grid = `repeat(` + x + `, 1fr)`;
     let container = document.getElementById("container");
     container.style.gridTemplateColumns = grid;
@@ -40,10 +68,10 @@ slider.addEventListener("change", function(){
     for(let i=0; i < (x*x); i++)
     {
     let newDiv = document.createElement('div');
-    newDiv. addEventListener("mouseover", () => newDiv.classList.add("colored"));
+    newDiv. addEventListener("mouseover", () => newDiv.style.backgroundColor = `rgb(${rSlider.value}, ${gSlider.value}, ${bSlider.value})`);
+    
     newDiv.className = 'cells';
     toAdd.appendChild(newDiv);
     }
     container.appendChild(toAdd);
 })
-
